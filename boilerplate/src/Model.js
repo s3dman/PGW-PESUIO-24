3a91d354155fc3d7fd6f21a4f06a09b0b9f7ae08
NevilleJS <69287021+s3dman@users.noreply.github.com>
2024-03-06 10:28:27 +0530

    added models

--- boilerplate/src/Model.js
+++ boilerplate/src/Model.js
@@ -31,3 +31,35 @@ export class Triangle extends Model {
 		this.gl.drawArrays(this.gl.TRIANGLES, 0, 3);
 	}
 }
+
+export class Square extends Model {
+	setup() {
+		const pos = new Float32Array([
+			-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.5, 0.5, 0.0, -0.5, -0.5, 0.0,
+			0.5, 0.5, 0.0, -0.5, 0.5, 0.0,
+		]);
+		const col = new Float32Array([
+			1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0,
+			0.0, 1.0, 1.0, 1.0, 0.0,
+		]);
+
+		this.vao = this.gl.createVertexArray();
+		this.gl.bindVertexArray(this.vao);
+
+		this.positionBuffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, pos, this.gl.STATIC_DRAW);
+		this.gl.enableVertexAttribArray(0);
+		this.gl.vertexAttribPointer(0, 3, this.gl.FLOAT, false, 0, 0);
+
+		this.colorBuffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, col, this.gl.STATIC_DRAW);
+		this.gl.enableVertexAttribArray(1);
+		this.gl.vertexAttribPointer(1, 3, this.gl.FLOAT, false, 0, 0);
+	}
+	render() {
+		this.gl.bindVertexArray(this.vao);
+		this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
+	}
+}
